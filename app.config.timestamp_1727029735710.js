090477e560e3917eca439ac5b57ea5707b674b6e
vasyaqwe <elekton.fafik@gmail.com>
2024-09-23 16:33:44 +0300

    format & rm custom caching

--- app.config.timestamp_1727029735710.js
+++ app.config.timestamp_1727029735710.js
@@ -1,15 +1,13 @@
 // app.config.ts
-import { defineConfig } from "@tanstack/start/config";
-import tsConfigPaths from "vite-tsconfig-paths";
-var app_config_default = defineConfig({
-  vite: {
-    plugins: () => [
-      tsConfigPaths({
-        projects: ["./tsconfig.json"]
-      })
-    ]
-  }
-});
-export {
-  app_config_default as default
-};
+import { defineConfig } from "@tanstack/start/config"
+import tsConfigPaths from "vite-tsconfig-paths"
+const app_config_default = defineConfig({
+   vite: {
+      plugins: () => [
+         tsConfigPaths({
+            projects: ["./tsconfig.json"],
+         }),
+      ],
+   },
+})
+export { app_config_default as default }
